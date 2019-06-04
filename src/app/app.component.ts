93c1522807b975ffed936ad83a0bd39716c1cd44
Aron Szabo <aronszabo@Arons-MacBook-Air.local>
2019-06-05 00:02:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Barcode, ScanSettings } from 'scandit-sdk';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'scandit-app';
+	accessCamera = true;
+	title = 'scandit-app';
+
+	public settings = new ScanSettings({ enabledSymbologies: [ Barcode.Symbology.CODE128 ] });
+
+	onScan(event) {
+		this.title = event.barcodes[0].data;
+	}
+
+	toggle() {
+		this.accessCamera = !this.accessCamera;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ScanditSdkModule } from 'scandit-sdk-angular';
+
 import { AppComponent } from './app.component';
 
+const KEY = '';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [ BrowserModule, ScanditSdkModule.forRoot(KEY, 'assets/') ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
